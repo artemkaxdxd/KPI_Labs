eb90ec6326a9e01f169187e7631dc094327becc4
Artem <bondarchuk.artem20@gmail.com>
2020-12-08 00:17:51 +0200

    Finished lab4

--- Lab4/Lab4.c
+++ Lab4/Lab4.c
@@ -8,7 +8,7 @@ int matrix[M][N] = {
         {  0, 3, 6, 9, 12, 16, 19, 22 },
         {  0, 3, 6, 8, 12, 16, 19, 22 },
         { -1, 3, 5, 8, 12, 15, 18, 21 },
-        { -1, 2, 5, 8, 11, 14, 18, 21 },
+        { -1, 2, 5, 8, 11, 15, 18, 21 },
         { -1, 2, 5, 8, 11, 14, 18, 21 },
         { -1, 2, 5, 7, 11, 14, 17, 20 },
         { -2, 1, 4, 7, 10, 14, 17, 20 },
@@ -16,14 +16,16 @@ int matrix[M][N] = {
         { -2, 1, 4, 7, 10, 13, 17, 20 } };
 
 int main(void) {
-    int col = 0, key;
-    printf("Input your key: ");
-    scanf_s("%d", &key);
-    if (key < -2 || key > 22) {
-        printf("Your key doesn't exist in matrix");
-        return 0;
+    for (int R = 0; R < M; R++) {
+        for (int C = 0; C < N; C++) {
+            printf("%d ", matrix[R][C]);
+        }
+        printf("\n");
     }
-    while (col < 8) {
+    int col = 0, key;
+    printf("\nInput your key: ");
+    scanf_s("%d", &key);
+    while (col < N) {
         int low = 0, high = 9, mid = (low + high) / 2;
         while (low <= high) {
             if (matrix[mid][col] == key) {
