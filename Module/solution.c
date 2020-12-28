5bc4d9c41eaf10678ebb3bc004cc82a5121c7308
Artem <bondarchuk.artem20@gmail.com>
2020-12-28 02:09:44 +0200

    Update solution.c

--- Module/solution.c
+++ Module/solution.c
@@ -3,22 +3,22 @@
 #define n 10
 
 int main() {
-   float X[n];
-   float Y[n];
-   float A = 5;
-   X[0] = A;
-   Y[0] = 1;
-   for (int i = 0; i < n; i++) {
-       float sum = 0;
-       for (int k = 0; k < n - 2; k++) {
-           sum += pow((1 - X[i]), k);
-        };
-       if (i + 1 < n) {
-           Y[i + 1] = Y[i] * sum;
-           X[i + 1] = X[i] * sum;
-           printf("%f\n", Y[i + 1]);
-           printf("%f\n", X[i + 1]);
-       }
-   }
-   return 0;
+    float X[n];
+    float Y[n];
+    float A = 5;
+    X[0] = A;
+    Y[0] = 1;
+    double power = 1;
+    for (int i = 0; i < n - 2; i++) {
+        float sum = 0;
+        power *= (1 - X[i]);
+        sum += power;
+        if (i + 1 < n) {
+            Y[i + 1] = Y[i] * sum;
+            X[i + 1] = X[i] * sum;
+            printf("%f\n", Y[i + 1]);
+            printf("%f\n", X[i + 1]);
+        }
+    }
+    return 0;
 };
