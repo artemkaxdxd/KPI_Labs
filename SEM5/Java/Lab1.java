116cfcba70cb85aad014f04aa93468b51fe0f160
Artem <bondarchuk.artem20@gmail.com>
2022-10-04 10:40:37 +0300

    corrected labs 1 and 2

--- SEM5/Java/Lab1.java
+++ SEM5/Java/Lab1.java
@@ -1,17 +1,24 @@
 public class Lab1 {
     public static void main(String[] args){
-        double a = 12;
-        double b = 13;
-        int n = 50;
-        int m = 51;
+        final double a = 3;
+        final double b = 1;
+        final int n = 5;
+        final int m = 2;
+        final int C3 = 2;
+
+        if (a <= C3 && n >= C3) {
+            System.out.println("Error! Division by 0!");
+            return;
+        }
+
         double sum = 0;
+        double denominator;
+        double numerator;
         for (double i = a; i <= n; i++) {
+            denominator = (i - C3);
             for (double j = b; j <= m; j++) {
-                double numerator = (i + j);
-                double denominator = (i - 2);
-                if (denominator != 0) {
-                    sum += numerator / denominator;
-                }
+                numerator = (i + j);
+                sum += numerator / denominator;
             }
         }
         System.out.println(sum);
--- SEM5/Java/Lab2.java
+++ SEM5/Java/Lab2.java
@@ -1,48 +1,62 @@
 public class Lab2 {
     public static void main(String[] args) {
-        float[][] matrix1 = {
+        final float[][] matrix1 = {
                 {0, 1, 2, 3},
-                {4, 5, 6, 7},
-                {8, 9, 5, 6} };
-        float[][] matrix2 = {
-                {9, 8},
-                {7, 6}
+                {4, 5, 6, 7, 3},
+                {8, 9, 5, 6}
         };
-        int lengthM1 = matrix1.length;
-        int lengthM2 = matrix2.length;
+        final float[][] matrix2 = {
+                {9, 8},
+                {7, 6,3}
+        };
+        final int columnsM1 = matrix1.length;
+        final int columnsM2 = matrix2.length;
+
         int rowsM1 = matrix1[0].length;
         int rowsM2 = matrix2[0].length;
+        int tempRows;
 
-        int n = lengthM1 + lengthM2;
-        int m = rowsM1 + rowsM2;
+        for (int i = 0; i < columnsM1; i++) {
+            tempRows = matrix1[i].length;
+            if (tempRows > rowsM1) {
+                rowsM1 = tempRows;
+            }
+        }
+        for (int i = 0; i < columnsM2; i++) {
+            tempRows = matrix2[i].length;
+            if (tempRows > rowsM2) {
+                rowsM2 = tempRows;
+            }
+        }
+        final int n = columnsM1 + columnsM2;
+        final int m = rowsM1 + rowsM2;
 
         float[][] matrix3 = new float[n][m];
-        for (int i = 0; i < lengthM1; i++) {
-            for (int j = 0; j < rowsM1; j++){
+        for (int i = 0; i < columnsM1; i++) {
+            for (int j = 0; j < matrix1[i].length; j++){
                 matrix3[i][j] = matrix1[i][j];
             }
         }
-        for (int i = 0; i < lengthM2; i++) {
-            for (int j = 0; j < rowsM2; j++) {
-                matrix3[i + lengthM1][j + rowsM1] = matrix2[i][j];
+        for (int i = 0; i < columnsM2; i++) {
+            for (int j = 0; j < matrix2[i].length; j++) {
+                matrix3[i + columnsM1][j + rowsM1] = matrix2[i][j];
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                System.out.print( matrix3[i][j]  + "\t");
+                System.out.print(matrix3[i][j]  + "\t");
             }
             System.out.println();
         }
 
-        float[] avg = new float[n];
+        float sum;
         for(int i = 0; i < n; i++) {
-            float sum = 0;
+            sum = 0;
             for(int j = 0; j < m; j++) {
                 sum += matrix3[i][j];
             }
-            avg[i] = sum / m;
-            System.out.printf("Average for %d row: %f\n", i,  avg[i]);
+            System.out.printf("Average for %d row: %f\n", i,  sum / m);
         }
     }
 }
